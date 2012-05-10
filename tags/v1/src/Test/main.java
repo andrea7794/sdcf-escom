14ce6fca44fdc1f6d44b9d2a369387ef16056728
pretxel100@gmail.com <pretxel100@gmail.com@b2b515e2-031d-24fd-c9b4-1021fdd50c48>
2012-05-10 00:07:15 +0000

    versión 1

---
+++ tags/v1/src/Test/main.java
@@ -0,0 +1,19 @@
+package Test;
+
+import Model.caract_knn;
+import Model.knn;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inicio");
+		knn ini = new knn(3);
+		System.out.println(ini.iniciar());
+		
+	}
+
+}
